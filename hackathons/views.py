d5bc3eeda3d5411f2195b66742d76e2c90d4a574
Shriratna <shriratna@gmail.com>
2024-09-29 19:34:03 +0530

    created User Token Generation Api

--- hackathons/views.py
+++ hackathons/views.py
@@ -8,6 +8,8 @@ from .serializers import HackathonSerializer, SubmissionSerializer
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.decorators import action
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
 
 
 class HackathonViewSet(viewsets.ModelViewSet):
@@ -32,3 +34,16 @@ class SubmissionViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+
+
+class CustomAuthToken(ObtainAuthToken):
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data, context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({
+            'token': token.key,
+            'user_id': user.pk,
+            'username': user.username
+        })
